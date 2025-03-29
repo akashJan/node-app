468311968f79c5a1d5293c74d16cbfd6e4fc58a7
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-03-30 01:09:27 +0530

    CRUD Ussing Express & Mongoose

--- express.js
+++ express.js
@@ -1,16 +1,59 @@
 const { request, response } = require("express");
 
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-// To retrieve or fetch the data
-app.get("/students", (request, response) => {
-  response.send(`Student data: ${request.url}`);
+// To parse the request bodies
+app.use(express.json());
+
+console.log(`===== Mongo DB connection =====`);
+// mongodb://localhost:27017/user_database
+async function mongoConnection() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/user_database");
+    console.log("====== Connection successfully ==========");
+  } catch (error) {
+    console.log(error);
+  }
+}
+mongoConnection();
+
+// Defining Schema for Student
+const schemaStudent = new mongoose.Schema(
+  {
+    name: { type: String },
+    college: { type: String },
+    marks: { type: String },
+  },
+  { collection: "student_collection" }
+);
+
+// Creating the Model for Schema: schemaStudent
+const Student = mongoose.model("Student", schemaStudent);
+
+// To create the resource
+app.post("/create", (request, response) => {
+  const { name, college, marks } = request.body;
+  Student.create({
+    name,
+    college,
+    marks,
+  });
+  response.send(`Successfully created Student `);
 });
 
-// To Create the resource
-app.post("/create", (request, response) => {
-  response.send(`Student created successfully: ${request.url}`);
+// // To retrieve or fetch the data
+// app.get("/students", (request, response) => {
+//   response.send(`Student data: ${request.url}`);
+// });
+
+//To retrieve or fetch the data
+app.get("/students", async (request, response) => {
+  const allStudents = await Student.find({});
+  response.status(200).json({
+    allStudents,
+  });
 });
 
 // To update the resource
